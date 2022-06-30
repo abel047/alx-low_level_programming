4f4b79a0025996caaf6392e905ef696a9bf9aca4
Abel <abelyoha7@gmail.com>
2022-06-30 10:05:41 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ 
+  * main - main block
+  * Return: 0
+ 
+ */
 
 int main(void)
 	
@@ -16,4 +15,4 @@ printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 
-}}
+}
