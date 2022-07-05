4c856c78b2cb1e569e602a5d81cc336340c006d8
abel047 <106524058+abel047@users.noreply.github.com>
2022-07-05 15:59:23 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,14 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+char c[] = "_putchar";
+  
+  for(int i=0; i<=8;i++)
+  {
+    __putchar(c[i]);
+  
+  }
+  __putchar('\n');
 
 return (0);
 }
