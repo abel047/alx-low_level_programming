f7e2f996bb17af347ef406702a3b97ef41bc526d
abel047 <106524058+abel047@users.noreply.github.com>
2022-07-11 18:34:33 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,13 @@
 /**
- * _strlen - funtion
- * @s: parameter
- * Return: lenth of sting
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
  */
-
 int _strlen(char *s)
 {
-return (strlen(s));
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
