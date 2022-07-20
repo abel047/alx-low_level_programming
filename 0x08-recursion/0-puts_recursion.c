28727d48ec6b355a3bc44eddb6a97f2c52b90279
Abel <abelyoha7@gmail.com>
2022-07-20 06:06:56 -0700

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check
+ * _puts_recursion - check
  * @s: character parameter
  */
 
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 if(*s != '\0')
 {
 _putchar(*s);
-_putchar(*s + 1);
+_puts_recursion(*s + 1);
 }
 else
 _putchar('\n');
