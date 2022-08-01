e5b33e62f804fa87ace29329f0916eaa1639613a
Abel <abelyoha7@gmail.com>
2022-08-01 03:05:38 -0700

    d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+truct dog - desc                                                                                                                  
+
+ * @name                                                                                                                              
+ *
+ *  * @age                                                                                                                               
+ *
+ *   * @owner                                                                                                                             
+ *
+ *    *                                                                                                                                    
+ *
+ *     *desc: blah                                                                                                                          
+ *
+ *      */                                                                                                                                   
+
+                                                                                                                                       
+
+ struct dog                                                                                                                            
+
+{                                                                                                                                     
+
+	 char *name;                                                                                                                          
+
+	  float age;                                                                                                                           
+
+	   char *owner;                                                                                                                         
+
+}             :wq
